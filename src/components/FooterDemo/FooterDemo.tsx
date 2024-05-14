8b0b99adc7964e5f86534b6f8ec8850898e54464
JoDcntrl <jodcntrl28@gmail.com>
2024-05-14 16:39:25 +0300

    DCNTRL-13 fiat add footerPopupDemo

--- src/components/FooterDemo/FooterDemo.tsx
+++ src/components/FooterDemo/FooterDemo.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import cn from "classnames";
 
 import { LinksArr, IconsArr } from "@/components/FooterDemo/FooterData";
 import { getYear } from "@/helpers/helpers";
 import { FooterDemoTypes } from "./Footer.types";
+import FooterPopupDemo from "./FooterPopupDemo/FooterPopupDemo";
 
 import { Logo } from "@/assets/svgs/Logo";
 
 import styles from "./footerDemo.module.scss";
+import { number } from "yup";
 
-const FooterDemo: React.FC<FooterDemoTypes> = ({ wide = false }) => (
+const FooterDemo: React.FC<FooterDemoTypes> = ({ wide = false }) => {
+  const [selectedLinkId, setSelectedLinkId] = useState<number | null>(null);
+
+  const closePopup = () => {
+    setSelectedLinkId(null);
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', closePopup);
+
+    return () => {
+      document.removeEventListener('click', closePopup);
+    }
+  }, []);
+
+  return (
   <footer className={styles.footerDemoMain}>
     <div className={cn(styles.footerDemo, { [styles.wide]: wide })}>
       <div className={styles.footerDemoLogoContainer}>
@@ -20,8 +39,15 @@ const FooterDemo: React.FC<FooterDemoTypes> = ({ wide = false }) => (
       <div className={styles.footerMenu}>
         <div className={styles.footerList}>
           {LinksArr.map(({ text, id, href }) => (
-            <Link key={id} className={styles.footerListLink} href={href}>
+            <Link 
+              key={id} 
+              href={href} 
+              className={styles.footerListLink} 
+              scroll = {false}
+              onMouseEnter={() => setSelectedLinkId(id === 5 ? id : null)}
+            > 
               {text}
+              {selectedLinkId === id  && <FooterPopupDemo closePopup={closePopup}/>}          
             </Link>
           ))}
         </div>
@@ -40,5 +66,9 @@ const FooterDemo: React.FC<FooterDemoTypes> = ({ wide = false }) => (
     </div>
   </footer>
 );
+}
 
 export default FooterDemo;
+
+
+
