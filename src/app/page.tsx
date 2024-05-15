43c55031e0838d066a51672481cc6399679f0b35
JoDcntrl <jodcntrl28@gmail.com>
2024-05-15 14:30:59 +0300

    dcntrl-14 fix review note

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,19 @@ import MainScreen from "@/components/MainScreen/MainScreen";
 import Layout from "@/components/Layout/Layout";
 import Banner from "@/components/Banner/Banner";
 import RegisteredVacancies from "@/components/RegisteredVacancies/RegisteredVacancies";
-import StoreProvider from "./StoreProvider";
 import RegisteredCompanies from "@/components/RegisteredСompanies/RegisteredCompanies";
 
 import styles from "./page.module.css";
 
 const Home = () => (
-  <StoreProvider>
-    <main className={styles.main}>
-      <Layout>
-        <MainScreen />
-        <RegisteredVacancies />
-        <Banner />
-        <RegisteredCompanies />
-      </Layout>
-    </main>
-  </StoreProvider>
+  <main className={styles.main}>
+    <Layout>
+      <MainScreen />
+      <RegisteredVacancies />
+      <Banner />
+      <RegisteredCompanies />
+    </Layout>
+  </main>
 );
 
 export default Home;
