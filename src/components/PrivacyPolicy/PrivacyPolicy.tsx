87d681e4ec24db7a0794b7066323e47bf6e49c15
JoDcntrl <jodcntrl28@gmail.com>
2024-05-16 17:14:36 +0300

    DCNTRL-16 fix scroll fix

--- src/components/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { PrivacyPolicyData } from './PrivacyPolicyData'
 import InformationPage from '../InformationPage /InformationPage'
 
 
 const PrivacyPolicy = () => {
+    window.scrollTo(0,0);
     return (
         <InformationPage currentPage='Privacy Policy' title='Privacy Policy' cards={PrivacyPolicyData}/>
     )
--- src/components/TermsConditions/TermsConditions.tsx
+++ src/components/TermsConditions/TermsConditions.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { TermsConditionsData } from "./TermsConditionsData";
 import InformationPage from "../InformationPage /InformationPage";
 
 const TermsConditions = () => {
+    window.scrollTo(0,0);
     return (
         <InformationPage currentPage='Terms & conditions' title='Terms & conditions' cards={TermsConditionsData}/>
     )
