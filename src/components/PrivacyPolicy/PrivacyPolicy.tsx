4397dcc6cd035f8512416e1f4e87a97c9fd62146
JoDcntrl <jodcntrl28@gmail.com>
2024-05-16 13:36:56 +0300

    DCNTRL-16 fix add universal component for information pages

--- src/components/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,39 +1,10 @@
-import Link from 'next/link'
 import { PrivacyPolicyData } from './PrivacyPolicyData'
-
-import styles from './privacyPolicy.module.scss'
+import InformationPage from '../InformationPage /InformationPage'
 
 
 const PrivacyPolicy = () => {
     return (
-        <main className={styles.main}>
-            <div className={styles.mainWrapper}>
-                <div className={styles.wrapperBlockLinks}>
-                    <Link className={styles.blockLink} href="/">
-                        Main
-                    </Link>
-                    <span className={styles.blockSlash}>/</span>
-                    <Link className={styles.blockLinkCurrent} href={""}>
-                        Privacy Policy
-                    </Link>
-                </div>
-                <h1 className={styles.mainTitle}>Privacy Policy</h1>
-                <div className={styles.vacancies}>
-                    {
-                        PrivacyPolicyData.map((vacancy) => (
-                            <div className= {styles.vacancy}>
-                                <h2 className= {styles.vacancyTitle}>{vacancy.title}</h2>
-                                <div className= {styles.vacancyText}>
-                                    {
-                                        vacancy.text.map(text => <p>{text}</p>)
-                                    }
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
-        </main> 
+        <InformationPage currentPage='Privacy Policy' title='Privacy Policy' cards={PrivacyPolicyData}/>
     )
 }
 
