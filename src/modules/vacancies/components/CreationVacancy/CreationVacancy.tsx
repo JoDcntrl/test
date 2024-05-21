f21c59c570eb387272cc71522b23c20b32152c74
Uqw <uqw@Mac-mini-montr.local>
2024-05-21 16:57:42 +0300

    DDCNTRL-6 fix review note(2)

--- src/modules/vacancies/components/CreationVacancy/CreationVacancy.tsx
+++ src/modules/vacancies/components/CreationVacancy/CreationVacancy.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { VacancyForm } from "@/modules/vacancies/components/vacancyForm/VacancyForm";
 
 import styles from "./styles.module.scss";
@@ -5,7 +7,19 @@ import styles from "./styles.module.scss";
 export const CreationVacancy = () => (
   <div className={styles.creation}>
     <div className={styles.creationWrapper}>
-      <div className={styles.breadCrumbsWrapper}></div>
+      <div className={styles.wrapperBlockLinks}>
+        <Link className={styles.blockLink} href="/">
+          Main
+        </Link>
+        <span className={styles.blockSlash}>/</span>
+        <Link className={styles.blockLink} href="/companies">
+          Companies
+        </Link>
+        <span className={styles.blockSlash}>/</span>
+        <Link className={styles.blockLinkCurrent} href={""}>
+          Stellar
+        </Link>
+      </div>
       <h1 className={styles.title}>Job creation</h1>
       <VacancyForm />
     </div>
